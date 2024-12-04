3cf88de5445b38df554d43c80b703ea76dc0dbc5
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2024-12-04 16:20:50 +0100

    correction base js

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 const lightbox = document.querySelector('#lightbox');
 const Lightboximg = document.querySelector('#lightbox img');
 document.querySelectorAll('[data-full-img]').forEach((imgThumb)=>{
-    imgThumb.addEventListener('toggle',(evt)=> {
+    imgThumb.addEventListener('click',(evt)=> {
         Lightboximg.src = imgThumb.dataset.fullImg;
         lightbox.showModal()   
     });
