36cece120edd1073dbd354827bfa494ffe5b952a
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2024-12-04 16:20:34 +0100

    js

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,11 @@
 // *** LIGHTBOX ***
 const lightbox = document.querySelector('#lightbox');
 const Lightboximg = document.querySelector('#lightbox img');
+document.querySelectorAll('[data-full-img]').forEach((imgThumb)=>{
+    imgThumb.addEventListener('toggle',(evt)=> {
+        Lightboximg.src = imgThumb.dataset.fullImg;
+        lightbox.showModal()   
+    });
+});
+
+
