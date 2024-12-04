1b161fe961a0b3064b8231911f43bf921c0ebf26
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2024-12-04 20:25:41 +0100

    vrai fin du tp

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 // *** LIGHTBOX ***
 const lightbox = document.querySelector('#lightbox');
-const lightboximg = document.querySelector('#lightbox img');
+const Lightboximg = document.querySelector('#lightbox img');
 document.querySelectorAll('[data-full-img]').forEach((imgThumb)=>{
     imgThumb.addEventListener('click',(evt)=> {
-        lightboximg.src = imgThumb.dataset.fullImg;
+        Lightboximg.src = imgThumb.dataset.fullImg;
         lightbox.showModal()   
     });
 });
